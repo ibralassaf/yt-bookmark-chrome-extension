3b8be2e16902885a622966b41d8cc4b7e7543d1e
Ibrahim Alassaf <xibra9141@gmail.com>
2022-05-28 17:05:29 +0300

    feat: update utils.js

--- utils.js
+++ utils.js
@@ -1,7 +1,9 @@
 //Get the current active tab
 export async function getActiveTabURL() {
-    let queryOptions = { active: true, lastFocusedWindow: true };
-    // `tab` will either be a `tabs.Tab` instance or `undefined`.
-    let [tab] = await chrome.tabs.query(queryOptions);
-    return tab;
+    const tabs = await chrome.tabs.query({
+        currentWindow: true,
+        active: true
+    });
+
+    return tabs[0];
 }
